f3832cfe6cb329d2dbb218530caaefaf192466a2
Noronha <inc.691@gmail.com>
2020-09-18 12:36:31 -0300

    Hover aside bar included

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 
 import logo_landing from '../../assets/logo-landing.png';
-import { ReactComponent as Logo_icon } from '../../assets/icons/login.svg';
-import { ReactComponent as Register_icon } from '../../assets/icons/register.svg';
+import { ReactComponent as LogoIcon } from '../../assets/icons/login.svg';
+import { ReactComponent as RegisterIcon } from '../../assets/icons/register.svg';
+import { ReactComponent as BgDecoration } from '../../assets/icons/bg-decoration.svg';
+import { ReactComponent as LogoAside } from '../../assets/icons/logo-aside.svg';
+import { ReactComponent as SearchIcon } from '../../assets/icons/search.svg';
+import { ReactComponent as RegisterApiIcon } from '../../assets/icons/register-api.svg';
+import { ReactComponent as DiscoverIcon } from '../../assets/icons/world.svg';
+import { ReactComponent as PayPalIcon } from '../../assets/icons/paypal.svg';
 
 import "./styles.css";
 
@@ -10,19 +16,46 @@ const Landing: React.FC = () => {
   return (
     <div className="landing-container" >
         <header>
-            <img src={logo_landing} alt="logo-image" className="logo-image"/>
+            <img src={logo_landing} alt="apicompassa" className="logo-image"/>
 
             <div className="user-buttons">
                 <div className="login btn">
-                    <Logo_icon className="icon" />
+                    <LogoIcon className="icon" />
                     Login
                 </div>
                 <div className="register btn">
-                    <Register_icon className="icon" />
+                    <RegisterIcon className="icon" />
                     Register
                 </div>
             </div>
         </header>
+
+        <aside className="aside-bar">
+            <LogoAside className="logo-aside" />
+
+            <label htmlFor="search-input" className="search-box">
+                <SearchIcon className="search-icon" />
+                <input id="search-input" type="text" placeholder="Search APIs..."/>
+            </label>
+
+            <div className="register-api aside-container">
+                <RegisterApiIcon className="aside-icon" />
+                <span>Register API</span>
+            </div>
+
+            <div className="register-api aside-container">
+                <DiscoverIcon className="aside-icon" />
+                <span>Discover</span>
+            </div>
+
+            <div className="donate-container">
+                <PayPalIcon className="paypal-icon" />
+                <span>Donate</span>
+            </div>
+
+        </aside>
+
+        <BgDecoration className="bg-decoration" />
     </div>
   );
 }
